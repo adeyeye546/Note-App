4e43f4a07bb0b174e52a0be5c795d9185beea255
Gbenga Sam <adeyeye546@gmail.com>
2018-08-06 01:03:47 +0100

    Adding and Saving Notes

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,10 +1,31 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
 
 var addNote = (title, body) => {
-  console.log('Adding notes', title, body);
-  
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
     
+  }
+
+  var duplicateNotes = notes.filter((note) =>  note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+
+
+  var notesString = fs.readFileSync('notes-data.json');
+  notes = JSON.parse(notesString);
+  
 };
 
 var getAll = () => {
@@ -13,7 +34,7 @@ var getAll = () => {
       
   };
   var getNote = (title) => {
-    console.log('Getting notes', title);
+    console.log('Getting note', title);
     
       
   };
