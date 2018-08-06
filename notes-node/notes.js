327580557454e54589d601f83d404143229dd5df
Gbenga Sam <adeyeye546@gmail.com>
2018-08-07 00:41:27 +0100

    Reading notes and Resuability

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -39,9 +39,9 @@ var getAll = () => {
       
   };
   var getNote = (title) => {
-    console.log('Getting note', title);
-    
-      
+    var notes = fetchNotes();
+    var filterNotes = notes.filter((note) => note.title === title);
+    return filterNotes[0]
   };
   var removeNote = (title) => {
     var notes = fetchNotes();
@@ -51,9 +51,16 @@ var getAll = () => {
     return notes.length !== removeNotes.length;
   };
 
+  var logNote = (note) => {
+    console.log('__');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  }
+
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 }
